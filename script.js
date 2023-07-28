3d775bf5510cdfbb10438887264d14b7c42d1362
Aathira P R <aadhirapr@gmail.com>
2023-07-28 20:53:15 +0530

    Learning js

--- script.js
+++ script.js
@@ -1,3 +1,118 @@
 // console.log("Hello Aathira");
 
+// var full_name ="Aathira";
+// var marks = 100;
+// // console.log(marks);
+// // console.log("marks");
+
+// // full_name = prompt("Enter the name");
+
+// console.log("Full name of user is", full_name);
+
+// console.log(`
+//     full namme is ${marks}
+// `)
+
+// var num1 = 1;
+// var num2 =2;
+// console.log(num1*num2);
+
+// var sellingPrice = 199;
+// var listingPrice = 799;
+// var discount = ((listingPrice - sellingPrice) / listingPrice) * 100;
+// console.log(discount);
+
+// var round = Math.round(discount);
+// console.log(round +"% off");
+
+// var user = "appu";
+
+// switch (user) {
+//     case "Aathira":
+//         console.log("goodgirl");
+//         break;
+//     case "doppu":
+//         console.log("badboy");
+//         break;
+//     case "appu":
+//         console.log("goodboy");
+//         break;
+//     default:
+//         break;
+// }
+
+// function sayHello(name){
+//     console.log(`hi there ${name}`);
+// }
+// sayHello("bubbbu");
+
+// function sayHello() {
+//     return "Hey";
+// }
+// var hello = sayHello();
+// console.log(hello);
+
+// function User(name, role) {
+//     switch (role) {
+//         case "admin":
+//             return `${name} you are an admin`
+//         case "subadmin":
+//             return `${name} you are a suubadmin`
+//         default:
+//             break;
+//     }
+// }
+// console.log(User("Aathira", "admin"));
+
+// var fullname = "Aathira";
+// console.log(fullname);
+
+// console.log("Hai");
+
+// const id = 8;
+// id = 5;
+// console.log(id);
+// var name = "aath";
+// console.log("is" + name);
+// +, -, /, *, %
+
+// var num1 = 2;
+// var num2 = 3.5;
+// var num3 = Math.round(num1+num2);
+// console.log(num3);
+
+// var x = true;
+// x ? console.log("true") : console.log("false");
+
+// null, NaN, 0, undefined falsy values
+
+function isEven(){
+    if(number % 2 == 0){
+        return `is even`;
+    }
+    return `is odd`;
+}
+var number = [1, 3, 5].every(isEven);
+console.log(number);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
