e2c1eb79056a7bbe185e9bb2687e4ecdf4bf8cdf
Aathira P R <aadhirapr@gmail.com>
2023-07-30 18:52:13 +0530

    working to flip the card connection of js

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
-const cards = document.querySelectorAll('.card');
-
-for (let card of cards) {
-  card.addEventListener('click', function(event) {
-    if (event.target.classList.contains('cover')) {
-      event.target.classList.add('flip');
-      event.target.classList.remove('cover');
-    }
-  });
-}
+const cards = document.querySelectorAll(".card");
+console.log("cards");
